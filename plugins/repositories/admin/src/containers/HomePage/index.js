2f5b89374349e547b7d345ce6779d190f7fb57de
Gustavo Manca <gustavo.manca@pixter.com.br>
2021-08-24 14:39:10 -0300

    refactor: change context from advanced react to personal github
    - fetch data from github repositories
    - create header and table components in home page

--- plugins/repositories/admin/src/containers/HomePage/index.js
+++ plugins/repositories/admin/src/containers/HomePage/index.js
@@ -1,19 +1,53 @@
-/*
- *
- * HomePage
- *
- */
+import React, { memo, useEffect, useState } from "react";
+import { Header } from "@buffetjs/custom";
+import { Table } from "@buffetjs/core";
+import styled from "styled-components";
+import axios from "axios";
 
-import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+const Wrapper = styled.div`
+  padding: 18px 30px;
+
+  p {
+    margin-top: 1rem;
+  }
+`;
+
+const headers = [
+  {
+    name: "Name",
+    value: "name",
+  },
+  {
+    name: "Description",
+    value: "description",
+  },
+  {
+    name: "Url",
+    value: "html_url",
+  },
+];
 
 const HomePage = () => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/gustavomanca/repos")
+      .then(({ data }) => setRows(data))
+      .catch((error) =>
+        strapi.notification.error(`Oops... GitHub API limit exceeded, ${error}`)
+      );
+  });
+
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
+    <Wrapper>
+      <Header
+        title={{ label: "Repositories" }}
+        content="A list of my public GitHub repositories"
+      />
+
+      <Table headers={headers} rows={rows} />
+    </Wrapper>
   );
 };
 
