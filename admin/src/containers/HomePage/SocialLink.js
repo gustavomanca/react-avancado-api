e2b67faa8dc7843165961d378d3582117edcd896
Gustavo Manca <gustavo.manca@pixter.com.br>
2021-08-24 13:52:42 -0300

    feat: custom content and add social links

---
+++ admin/src/containers/HomePage/SocialLink.js
@@ -0,0 +1,44 @@
+import React, { memo } from "react";
+import PropTypes from "prop-types";
+
+import GitHub from "../../assets/images/social_gh.png";
+import Slack from "../../assets/images/social_slack.png";
+import Medium from "../../assets/images/social_medium.png";
+import Twitter from "../../assets/images/social_twitter.png";
+import Reddit from "../../assets/images/social_reddit.png";
+import Instagram from "../../assets/images/brands/instagram.png";
+import LinkedIn from "../../assets/images/brands/linkedin.png";
+
+import { SocialLinkWrapper } from "./components";
+
+function getSrc(name) {
+  const source = {
+    GitHub,
+    Instagram,
+    LinkedIn,
+    Reddit,
+    Medium,
+    Slack,
+    Twitter,
+  }[name];
+
+  return source || GitHub;
+}
+
+const SocialLink = ({ link, name }) => {
+  return (
+    <SocialLinkWrapper className="col-6">
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <img src={getSrc(name)} alt={name} />
+        <span>{name}</span>
+      </a>
+    </SocialLinkWrapper>
+  );
+};
+
+SocialLink.propTypes = {
+  link: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default memo(SocialLink);
