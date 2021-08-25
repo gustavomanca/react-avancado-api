7b0e71366d16908db05a8b765d61ebafc2e1857c
Gustavo Manca <gustavo.manca@pixter.com.br>
2021-08-25 11:03:30 -0300

    feat: create first game description scraping data service

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -1,8 +1,36 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
+const axios = require("axios");
 
-module.exports = {};
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+  const { document } = dom.window;
+
+  const description = document.querySelector(".description");
+
+  return {
+    rating: "BR0",
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML,
+  };
+}
+
+const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+
+module.exports = {
+  populate: async (params) => {
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+
+    console.log(await getGameInfo(products[0].slug));
+  },
+};
